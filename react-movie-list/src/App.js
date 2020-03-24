01a20e4a929a7f6cfad425571f92a21430657b4b
Oksana <ksenia.barshchyk@yandex.ru>
2020-03-24 23:15:47 +0200

    added rating

--- react-movie-list/src/App.js
+++ react-movie-list/src/App.js
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <div className="App">
-        <Container>
+        <Container style={{ marginTop:40 }}>
             <MovieForm />
             <Movies movies={ movies }/>
         </Container>
--- react-movie-list/src/MovieForm.js
+++ react-movie-list/src/MovieForm.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
-import { Form, Input } from 'semantic-ui-react';
+import { Form, Input, Rating } from 'semantic-ui-react';
 
 export const MovieForm = () => {
     const [title, setTitle] = useState('');
-
+    const [rating, setRating] = useState(1);
     return (
         <Form>
         <Form.Field>
-            <Input value={title} placeholder="movie title" onChange={e => setTitle(e.targer.value)} />
+            <Input value={title} placeholder="movie title" onChange={e => setTitle(e.target.value)} />
+        </Form.Field>
+         <Form.Field>
+          <Rating icon='star' value={rating} maxRating={5} onRate={(_, data) =>
+          {console.log(data);}
+          }/>
         </Form.Field>
         </Form>)
 }
